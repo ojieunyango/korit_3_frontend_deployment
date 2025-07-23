52b35204337351f269dafebef75b9a190c6a28dd
ojieunyango <ojieunyango@gmail.com>
2025-07-23 12:14:01 +0900

    modified

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/carlist_front/',
+  base: '/korit_3_frontend_deployment/',
   plugins: [react()],
 });
 
